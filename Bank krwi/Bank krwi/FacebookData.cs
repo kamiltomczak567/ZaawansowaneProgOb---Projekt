ede00636327924cd6e1036fc33b0419ae63e9cea
kamiltomczak567 <kamil.tomczak567@googlemail.com>
2017-05-25 19:46:21 +0200

    3commit

---
+++ Bank krwi/Bank krwi/FacebookData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_krwi
+{
+   public class FacebookData
+    {
+        public FacebookPost[] data { get; set; } 
+    }
+    public class Result
+    {
+        public FacebookData[] data { get; set; }
+    }
+}
--- Bank krwi/Bank krwi/MainWindow.xaml.cs
+++ Bank krwi/Bank krwi/MainWindow.xaml.cs
@@ -12,6 +12,13 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.CSharp;
+using System.Data;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+
 
 namespace Bank_krwi
 {
@@ -48,5 +55,38 @@ namespace Bank_krwi
            
 
         }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            string token = getToken();
+            var request = (HttpWebRequest)WebRequest.Create("https://graph.facebook.com/v2.9/wetbankkrwi/feed?access_token=" + token);
+            var response = (HttpWebResponse)request.GetResponse();
+            FacebookPost fbPost = getFacebookPost(response);
+            facebookPosts facebookPost = new facebookPosts(fbPost);
+            facebookPost.Show();
+        }
+
+        private string getToken()
+        {
+            var tokenRequest = (HttpWebRequest)WebRequest.Create("https://graph.facebook.com/v2.9/oauth/access_token?client_id=1066391300160675&client_secret=529c6b46acc37b676b6bc438c7691291&grant_type=client_credentials");
+            var tokenResponse = (HttpWebResponse)tokenRequest.GetResponse();
+            var responseTokenString = new StreamReader(tokenResponse.GetResponseStream()).ReadToEnd();
+            dynamic y = JsonConvert.DeserializeObject(responseTokenString);
+
+            return y.access_token;
+        }
+
+        private FacebookPost getFacebookPost(HttpWebResponse response)
+        {
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            dynamic x = JsonConvert.DeserializeObject(responseString);
+            var data = x.data;
+            string message = data[0].message;
+            string created_time = data[0].created_time;
+            string id = data[0].id;
+            FacebookPost fbPost = new FacebookPost(created_time, message, id);
+            return fbPost;
+        }
+
     }
 }
