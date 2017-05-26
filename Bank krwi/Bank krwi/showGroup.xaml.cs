0afc2cf85f4b0d6096ab383369ec42c4898005c5
JankowskiPiotr <jankowski.piotr@o2.pl>
2017-05-26 14:10:48 +0200

    Naprawa wyswietlania grup krwi

--- Bank krwi/Bank krwi/showGroup.xaml.cs
+++ Bank krwi/Bank krwi/showGroup.xaml.cs
@@ -26,60 +26,60 @@ namespace Bank_krwi
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Click_0Rh0(object sender, RoutedEventArgs e)
         {
             string a = "0Rh-";
             showUser showUser = new showUser(a);
             showUser.Show();
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void Click_0Rh1(object sender, RoutedEventArgs e)
         {
             string a = "0Rh+";
             showUser showUser = new showUser(a);
             showUser.Show();
         }
 
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-            string a = "BRh−";
-            showUser showUser = new showUser(a);
-            showUser.Show();
-        }
-
-        private void Button_Click_3(object sender, RoutedEventArgs e)
+        private void Click_BRh1(object sender, RoutedEventArgs e)
         {
             string a = "BRh+";
             showUser showUser = new showUser(a);
             showUser.Show();
-
         }
 
-        private void Button_Click_4(object sender, RoutedEventArgs e)
+        private void Click_BRh0(object sender, RoutedEventArgs e)
         {
-            string a = "ARh−";
+            string a = "BRh-";
             showUser showUser = new showUser(a);
             showUser.Show();
+
         }
 
-        private void Button_Click_5(object sender, RoutedEventArgs e)
+        private void Click_ARh1(object sender, RoutedEventArgs e)
         {
             string a = "ARh+";
             showUser showUser = new showUser(a);
             showUser.Show();
+        }
+
+        private void Click_ARh0(object sender, RoutedEventArgs e)
+        {
+            string a = "ARh-";
+            showUser showUser = new showUser(a);
+            showUser.Show();
 
         }
 
-        private void Button_Click_6(object sender, RoutedEventArgs e)
+        private void Click_ABRh1(object sender, RoutedEventArgs e)
         {
-            string a = "ABRh−";
+            string a = "ABRh+";
             showUser showUser = new showUser(a);
             showUser.Show();
         }
 
-        private void Button_Click_7(object sender, RoutedEventArgs e)
+        private void Click_ABRh0(object sender, RoutedEventArgs e)
         {
-            string a = "ABRh+";
+            string a = "ABRh-";
             showUser showUser = new showUser(a);
             showUser.Show();
         }
