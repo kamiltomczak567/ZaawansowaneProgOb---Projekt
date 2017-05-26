14c527ccd8c2db7a86bb7d2331b038a097bb0b26
Damian Osiecki <dam326@o2.pl>
2017-05-26 12:18:54 +0200

    PDF z bibliotekami nie skonczony

--- Bank krwi/Bank krwi/showGroup.xaml.cs
+++ Bank krwi/Bank krwi/showGroup.xaml.cs
@@ -6,11 +6,13 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
 
 namespace Bank_krwi
 {
@@ -80,6 +82,19 @@ namespace Bank_krwi
             string a = "ABRh+";
             showUser showUser = new showUser(a);
             showUser.Show();
+        }
+
+        private void B_pdf_Click(object sender, RoutedEventArgs e)
+        {
+            Document doc = new Document(iTextSharp.text.PageSize.LETTER, 10, 10, 42, 35);
+            PdfWriter wri = PdfWriter.GetInstance(doc, new FileStream("Test.pdf", FileMode.Create));
+            doc.Open(); // otworz dokument
+            //Zaawartosc dokumentu
+            Paragraph paragraph = new Paragraph("Proba pdfa");
+            doc.Add(paragraph);
+
+            doc.Close();
+
         }
         //0Rh− 	0Rh+ 	BRh− 	BRh+ 	ARh− 	ARh+ 	ABRh− 	ABRh+
     }
